aa4ce90c0e7fd65e2b1f497bf785c723fd5ab6fc
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-12-08 14:44:09 +0000

    accidentally lost my API example file, so reconstructed API from ground up. yikes

--- lib/subway/api.rb
+++ lib/subway/api.rb
@@ -1,3 +1,39 @@
-class API
+require 'net/http'
+
+#class API
   
-end 
+uri = URI('https://api.wmata.com/Incidents.svc/json/ElevatorIncidents')
+
+ # Request parameters if needed them by putting this into coad below 'StationCode' => '{string}, but don't want station paramters because we want ALL the outages of all the escalators and elevators
+
+query = URI.encode_www_form({})
+      
+
+# I think this goes through thing character by character, lol. Whoa...
+
+if uri.query && uri.query.length > 0
+    uri.query += '&' + query
+else
+    uri.query = query
+end
+
+request = Net::HTTP::Get.new(uri.request_uri)
+# Request headers
+request['api_key'] = '365be576f6f24a35b7b226a751b3c476' # Yeah, I had to sign up to get this key. It ws worth it, so I didn't have to deal with data limitations from the demo key 
+# Request body
+
+request.body = "{body}"
+
+response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme ==  'https') do |http|
+    http.request(request)
+end
+
+outages = response.body.split
+
+puts outages[0..100]
+
+#outages.each do | res |
+# p res
+#end
+  
+#end 
