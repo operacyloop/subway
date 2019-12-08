48aabd5445e3f35e784ff2c277ffffbc6537da21
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-12-08 07:40:15 +0000

    beefed up CLI class

--- lib/subway/cli.rb
+++ lib/subway/cli.rb
@@ -1,7 +1,49 @@
 class CLI
   
-  def self.testing
-   puts "I'm in the CLI Class"
+def self.menu 
+  puts "in the middle of the menu"
+  input = nil
+  while input != "exit"
+    puts "Enter the number of the Issue you'd like more information on or type list to see the issues again or exit:"
+    input = gets.strip.downcase
+    
+   # #The following is the logic dealing with how to deal with the user input
+   # # Also, a string evaluates to zero until it's a string number "8" = 8
+ 
+    if input.to_i > 0 
+      puts @issues[input.to_i-1] #subtract 1 so can find itin the array
+    elsif input == "list"
+      list_of_issues
+    end 
   end 
+     
+  #   # end 
+  #   # case input 
+  #   # when "1"
+  #   #   puts "More info on Elevator Issues..."
+  #   # when "2"
+  #   #   puts "More info on Escalator Issues..."
+  #   # when "list"
+  #   #   list_issues
   
 end 
+  
+  def self.goodbye 
+   puts "goodbye!"
+  end 
+
+    def self.list_of_issues
+      puts "Current Metrorail Issues:"
+    end 
+    
+  # # what follows is a class method and self is refering the class itself (aka the CLI class)...
+    def self.call
+      puts "in the middle of the call method, a class method"
+    end 
+    
+     list_of_issues
+     menu
+     goodbye
+  
+
+end
