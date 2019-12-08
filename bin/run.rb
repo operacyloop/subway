002c51764cb5445514b99a23e2c4a3fdac67d5a5
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-12-08 08:09:39 +0000

    continued test on CLI class

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby 
 # uncomment above when you take off the .rb (since it's in the bin folder)
 
-#require_relative '../lib/subway'
 require_relative '../lib/subway'
+require "bundler/setup"
+require "subway"
 
 puts "Hello World!"
 
--- lib/subway/issue.rb
+++ lib/subway/issue.rb
@@ -1,3 +1,33 @@
-class Issue
-
-end
+class Issue 
+  attr_accessor :name, :location, :problem, :exactlocation, :fixedestimate
+  
+  def self.current 
+     # I should return a bunch of instances of Issue 
+     #   puts "1. Elevator Issues"
+     #   puts "2. Escalator Issues"
+     # end 
+  issue_1 = self.new # same as... Issue.new 
+  issue_1.name = "Elevator Issue"
+  issue_1.location = "Anacostia"
+  issue_1.problem = "Minor Repair"
+  issue_1.exactlocation = "under the stairway before the street"
+  issue_1.fixedestimate = "Noon on January 30, 2020"
+  
+  issue_2 = self.new # same as... Issue.new 
+  issue_2.name = "Escalator Issue"
+  issue_2.location = "Congress Heights"
+  issue_2.problem = "Modernization"
+  issue_2.exactlocation = "Southside of the stairway before the street"
+  issue_2.fixedestimate = "Noon on December 30, 2019"
+  
+  # should return an array [issue_1, issue_2]
+  
+  end
+  
+#   What is an issue
+# -- Problem (aka SymptomDescription)
+# -- Subway Location (aka StationName) +
+# -- Equipment (aka UnitType)
+# -- Exact location (aka LocationDescription)
+# -- When Fixed (aka EstimatedReturnToService)
+  
