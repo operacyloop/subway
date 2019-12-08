c1eb86369ae2d2b56c26d6c04bc64e265082546e
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-12-08 06:33:11 +0000

    added the API class, and created independencies

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,7 @@
-# #!/usr/bin/env ruby 
+#!/usr/bin/env ruby 
 # uncomment above when you take off the .rb (since it's in the bin folder)
 
+#require_relative '../lib/subway'
 require_relative '../lib/subway'
 
 puts "Hello World!"
