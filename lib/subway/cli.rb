bfd2b378ea5b2d08c35399968cb0399a6a75d3a6
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-12-16 18:01:34 +0000

    Working on Logic in CLI, no errors

--- lib/subway/cli.rb
+++ lib/subway/cli.rb
@@ -1,53 +1,49 @@
 class CLI
   
-#######################################################################
-#########  THIS PART PROMPS AND GETS INPUT FROM A HUMAN  ##############
-#######################################################################
-  
   def self.menu 
-     input = nil
-     while input != "exit"
-       puts "Welcome to your go-to location for DC Subway Elevator and Escalator outages."
-       puts "1 - Elevators"
-       puts "2 - Escalators"
-       puts "Which would you like to get more information about? Type 'exit' to leave the program. Type list to see this again."
+#     input = nil
+#     while input != "exit"
+#       puts "Welcome to your go-to location for DC Subway Elevator and Escalator outages."
+#       puts "1 - Elevators"
+#       puts "2 - Escalators"
+#       puts "Which would you like to get more information about? Type 'exit' to leave the program. Type list to see this again."
     
-       input = gets.strip.downcase
+#       input = gets.strip.downcase
     
-# #######################################################################
-# ######### THIS PART THAT DEALS WITH LOGIC IN THE PROGRAM ##############
-# #######################################################################
+#################################################################
+### THIS PART THAT DEALS WITH LOGIC IN THE PROGRAM ##############
+#################################################################
     
-      if input == "1" 
-        puts "Elevator Issues"
-      if input == "2"
-         puts "Escalator Issues"
-      # elsif input == "list"
-      #   list_of_issues 
-      # else
-      #   puts "Not sure what you want"
-        goodbye
+#       if input == "1" 
+#         puts "Elevator Issues"
+#       if input == "2"
+#         puts "Escalator Issues"
+#       # elsif input == "list"
+#       #   list_of_issues 
+#       # else
+#       #   puts "Not sure what you want"
+#         goodbye
      end 
      
-#   #   ##if input != "1" or "2"
-#   # ##   puts "Please choose 1 or 2"
-#   #   # if input.to_i > 0 
-#   #   #   the_issue = @issues[input.to_i-1] #subtract 1 so can find itin the array
-#   #   #   puts "#{issue.name}" #all the attributes here
-#   #   # elsif input == "list"
-#   #   #   list_of_issues
-#   #   # else 
-#   #   #   puts "not what you want"
-#   #   #case input 
-#   #   #when "1"
-#   #   #  puts "More info on Elevator Issues..."
-#   #   #when "2"
-#   #   #  puts "More info on Escalator Issues..."
-#   # # when "list"
-#   #   # list_issues
-#   #   end 
-end 
-end 
+# #   #   ##if input != "1" or "2"
+# #   # ##   puts "Please choose 1 or 2"
+# #   #   # if input.to_i > 0 
+# #   #   #   the_issue = @issues[input.to_i-1] #subtract 1 so can find itin the array
+# #   #   #   puts "#{issue.name}" #all the attributes here
+# #   #   # elsif input == "list"
+# #   #   #   list_of_issues
+# #   #   # else 
+# #   #   #   puts "not what you want"
+# #   #   #case input 
+# #   #   #when "1"
+# #   #   #  puts "More info on Elevator Issues..."
+# #   #   #when "2"
+# #   #   #  puts "More info on Escalator Issues..."
+# #   # # when "list"
+# #   #   # list_issues
+# #   #   end 
+# end 
+# end 
   
   def self.goodbye 
     puts "Goodbye! Come back later for update escaltor and elevator outages"
@@ -56,14 +52,14 @@ end
   def self.list_of_issues
     @issues = issue.current 
     @issues.each.with_index(1) do | issue, i | #starts indexing at 1 
-       puts "#{i}. #{issue}"
-       puts "Current Metrorail Issues:"
-       puts # issue_1
-       puts # issue_2
+      puts "#{i}. #{issue}"
+      puts "Current Metrorail Issues:"
+      puts # issue_1
+      puts # issue_2
     end 
   end 
   
-  # # what follows is a class method and self is refering the class itself (aka the CLI class)...
+   # # what follows is a class method and self is refering the class itself (aka the CLI class)...
     
   def self.call
     puts "in the middle of the call method, a class method"
