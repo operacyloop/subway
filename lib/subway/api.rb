a01fc0e6845b7fffca6e509de924dbbac4fffc2e
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-12-17 14:08:03 +0000

    working API to get it running

--- lib/subway/api.rb
+++ lib/subway/api.rb
@@ -1,21 +1,21 @@
-#class API
-
 require 'net/http'
 require 'httparty'
 
+class API
+  
 uri = URI('https://api.wmata.com/Incidents.svc/json/ElevatorIncidents')
 
- # Request parameters if needed them by putting this into coad below 'StationCode' => '{string}, but don't want station paramters because we want ALL the outages of all the escalators and elevators
+# Request parameters if needed them by putting this into coad below 'StationCode' => '{string}, but don't want station paramters because we want ALL the outages of all the escalators and elevators
 
 query = URI.encode_www_form({})
       
 # The following code is from WMATA (DC Subway). I think this goes through thing character by character, lol. Whoa...
 
-if uri.query && uri.query.length > 0
+  if uri.query && uri.query.length > 0
     uri.query += '&' + query
-else
+  else
     uri.query = query
-end
+  end
 
 request = Net::HTTP::Get.new(uri.request_uri)
 # Request headers
@@ -25,7 +25,7 @@ request['api_key'] = '365be576f6f24a35b7b226a751b3c476' # Yeah, I had to sign up
 request.body = "{body}"
 
 response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme ==  'https') do |http|
-    http.request(request)
+  http.request(request)
 end
 
 #lets see what data is in here for now...
@@ -33,60 +33,51 @@ outages = response.body
 x = outages.split("},").to_a
 # puts x.class # not needed any more, this was for testing purposes
 
-
 count = 0
 
 while count <= x.length-2
-    issue = x[count].delete! '{}:'
-    issue = issue.split(",")
-    count += 1
+     issue = x[count].delete! '{}:'
+     issue = issue.split(",")
+     count += 1
+     puts "Issue is an #{issue.class}."
+     equipment = "Equipment: #{issue[1][10..100].delete('"').capitalize}"
+     puts equipment
     
-    puts "Issue is an #{issue.class}."
+     station = "Station Name: #{issue[4][13..100].delete('"')}"
+     puts station
     
+     location = "Location: #{issue[5].delete('"')[19..100]}"
+     puts location 
     
+     reason = "Reason for outage: #{issue[8][20..100].delete('"')}"
+     puts reason 
     
-    equipment = "Equipment: #{issue[1][10..100].delete('"').capitalize}"
-    puts equipment
+     eta = "Anticipated repair date: #{issue[10][15..25].delete('"')}" 
+     puts eta
+     puts eta.class
     
-    station = "Station Name: #{issue[4][13..100].delete('"')}"
-    puts station
-    
-    location = "Location: #{issue[5].delete('"')[19..100]}"
-    puts location 
-    
-    reason = "Reason for outage: #{issue[8][20..100].delete('"')}"
-    puts reason 
-    
-    eta = "Anticipated repair date: #{issue[10][15..25].delete('"')}" 
-    puts eta
-    puts eta.class
-    
-    # @@collection_of_issues = [ ]
-    issue = Issue.new(3, {})
-    @@collection_of_issues << issue
-    p issue
-    p issue.class
+     # @@collection_of_issues = [ ]
+     # issue = Issue.new(3, {})
+     # @@collection_of_issues << issue
+     p issue
+     p issue.class
    
-    puts @@collection_of_issues
-    puts @@collection_of_issues.class
-    puts @@collection_of_issues.length
-    puts
-  end
-
+     # puts @@collection_of_issues
+     # puts @@collection_of_issues.class
+     # puts @@collection_of_issues.length
+     puts
+   end
   
- 
-# end 
+end 
 
 
- class Issue 
-   attr_accessor :equipment, :station, :location, :reason, :eta
-  
-    def initialize 
-      @equipment_a = @equipment
-      @station_a = @station 
-      @location_a = @location 
-      @reason_a = @reason 
-      @eta_a = @eta 
-    end 
-    
-  end 
+# class Issue 
+#   attr_accessor :equipment, :station, :location, :reason, :eta
+#     def initialize 
+#       @equipment_a = @equipment
+#       @station_a = @station 
+#       @location_a = @location 
+#       @reason_a = @reason 
+#       @eta_a = @eta 
+#     end 
+#   end 
